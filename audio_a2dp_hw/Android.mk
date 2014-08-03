c156dc0894ec182eec840e83936c50532d946ea9
Ivan Zaitsev <ivan.zaitsev@gmail.com>
2014-08-02 18:36:23 -0700

    Switch to CM10.1 branch.

--- audio_a2dp_hw/Android.mk
+++ audio_a2dp_hw/Android.mk
@@ -2,13 +2,22 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
+ifeq ($(BOARD_USES_ALSA_AUDIO),true)
+ifeq ($(TARGET_QCOM_AUDIO_VARIANT),caf)
+	LOCAL_CFLAGS += -DSAMPLE_RATE_48K
+endif
+endif
+
 LOCAL_SRC_FILES:= \
 	audio_a2dp_hw.c
 
-LOCAL_C_INCLUDES+= .
+LOCAL_CFLAGS += $(bdroid_CFLAGS)
+
+LOCAL_C_INCLUDES+= . \
+	$(bdroid_C_INCLUDES)
 
 LOCAL_SHARED_LIBRARIES := \
-	libcutils liblog
+	libcutils
 
 LOCAL_SHARED_LIBRARIES += \
 	libpower
